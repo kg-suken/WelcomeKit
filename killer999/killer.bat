42658d362670f30dea3fc34adc2a1418c70bc823
roistaff <roistaff1983@gmail.com>
2025-08-05 12:36:09 +0900

    update

--- killer999/killer.bat
+++ killer999/killer.bat
@@ -1,8 +1,8 @@
 @echo off
 :loop
-taskkill /im supertuxkart.exe /F /T
-taskkill /im Minecraft.exe /F /T
-taskkill /im EpicGamesLauncher.exe /F /T
+taskkill /im supertuxkart.exe /F /T > null2>&1
+taskkill /im Minecraft.exe /F /T > null 2>&1
+taskkill /im EpicGamesLauncher.exe /F /T > null 2>&1
 timeout /t 1 >nul
 goto loop
 
